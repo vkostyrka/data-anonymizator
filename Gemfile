eb4c7872d34d4637df99338166b4e859291f000c
vkostyrka <vkosty@softserveinc.com>
2021-11-06 17:52:59 +0200

    add rspec

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails'
 end
 
 group :development do
--- test/controllers/database_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class DatabaseControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
