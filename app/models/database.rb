03b6617fd106e1a0b8fe3e8e49b823782d5c122e
vkostyrka <vkosty@softserveinc.com>
2021-11-07 14:28:11 +0200

    use sqlite gem to read tables

--- app/models/database.rb
+++ app/models/database.rb
@@ -4,4 +4,12 @@ class Database < ApplicationRecord
   belongs_to :user
   enum dbms_type: { sqlite: 0 }
   mount_uploader :file, FileUploader
+
+  def get_sqlite
+    SQLite3::Database.new(self.file.file.file)
+  end
+
+  def table_names
+    get_sqlite.execute("SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%';").flatten
+  end
 end
